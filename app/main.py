d595ad4df66ea8bc19c4aabd69f4d66190448508
Sasakura-ayato <sasakura.ayato.syosetu@gmail.com>
2022-07-08 23:42:33 +0900

    Replace Return Message to function output

--- app/main.py
+++ app/main.py
@@ -4,9 +4,32 @@ from fastapi import FastAPI
 from typing import Union
 import datetime
 import sqlite3
+import vlc
 
+# API サーバーの起動
 app = FastAPI()
 version = "1.0.0"
+
+# VLC インスタンスの起動
+p = vlc.MediaPlayer()
+
+# 標準エラーメッセージの定義
+def success_message(message: str = 'Success'):
+    return {
+        "success": "true",
+        "version": version,
+        "ReceivingDate": datetime.datetime.now(),
+        "message": message
+    }
+
+def failed_empty_parameters_message(parameters: str = 'Failed'):
+    return {
+        "success": "false",
+        "version": version,
+        "ReceivingDate": datetime.datetime.now(),
+        "message": "parameter " + parameters + " is(are) empty.",
+    }
+
 
 
 def cv_dict(cursor, row):
@@ -43,12 +66,7 @@ async def root(type: Union[str, None] = None, q: Union[str, None] = None):
     cur = conn.cursor()
 
     if type == None or q == None:
-        return {
-            "success": "false",
-            "version": version,
-            "ReceivingDate": datetime.datetime.now(),
-            "message": "parameter `type` or `q` is empty.",
-        }
+        return failed_empty_parameters_message('`type` or `q`')
 
     cur.execute("SELECT * FROM music_metadata WHERE " + type + " LIKE '%" + q + "%'")
     result = cur.fetchall()
@@ -60,3 +78,27 @@ async def root(type: Union[str, None] = None, q: Union[str, None] = None):
         "ReceivingDate": datetime.datetime.now(),
         "data": result,
     }
+
+@app.get("/status")
+async def root(type: Union[str, None] = None):
+    status = p.get_media_player()
+    return {
+        "track-index": status.get_media(),
+        "time": str(status.get_time() / 1000),
+        "mrl": status.get_mrl()
+    }
+
+@app.post("/vlc/play")
+async def root(path: Union[str, None] = None):
+    if path == None:
+        return failed_empty_parameters_message('`path`')
+
+    p.set_mrl(path)
+    p.play()
+
+    return success_message("VLC Started")
+
+@app.post("/vlc/stop")
+async def root():
+    p.stop()
+    return success_message("VLC Stopped")
